c89b520fe6f14bc6a5cad4799de83d55853ad305
AmirDapl <Amir@daplcorp.com>
2024-05-06 13:46:56 -0500

    update logic
    Added a preflight check to make sure everything is correct before letitng someone click copy.  Enhanced validation so empty lines won't break the logic for recipients.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import PaymentSplitter from "./components/PaymentSplitter";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-1 mx-auto">
       <PaymentSplitter />
+      <Analytics />
     </main>
   );
 }
--- src/components/transactionUtils.js
+++ src/components/transactionUtils.js
@@ -1,26 +1,41 @@
-import { Pact } from "@kadena/client";
+import { Pact, createClient } from "@kadena/client";
 
 export const generateTransaction = async (formData) => {
-  const { payees, sender, chain, network, splitOption, amount, selectedKeys, fungible } = formData;
-  // console.log("selectedKeys", selectedKeys);
-  const payeeArray = payees.split("\n");
+  const {
+    payees,
+    sender,
+    chain,
+    network,
+    splitOption,
+    amount,
+    selectedKeys,
+    fungible,
+  } = formData;
+  const apiUrl =
+    network === "testnet04"
+      ? "https://api.testnet.chainweb.com"
+      : "https://api.chainweb.com";
+  const pc = createClient(
+    `${apiUrl}/chainweb/0.0/${network}/chain/${chain}/pact`
+  );
+  const payeeArray = payees.split("\n").filter((payee) => payee.trim() !== "");
   const numPayees = payeeArray.length;
-
   let splitAmount;
   if (splitOption === "equal") {
     splitAmount = parseFloat(amount) / numPayees;
   } else {
-    const amountArray = amount.split("\n");
+    const amountArray = amount.split("\\n");
     splitAmount = amountArray.map(parseFloat);
   }
 
-  const senderPubKeys = sender.startsWith("k:") ? [sender.slice(2)] : selectedKeys || [];
+  const senderPubKeys = sender.startsWith("k:")
+    ? [sender.slice(2)]
+    : selectedKeys || [];
 
   let code = "";
   for (let i = 0; i < payeeArray.length; i++) {
     const payee = payeeArray[i];
     const pubKey = payee.startsWith("k:") ? payee.slice(2) : "";
-    // const transferAmount = splitAmount[i] || splitAmount;
     code += `(${fungible}.transfer-create "${sender}" "${payee}" (read-keyset "${pubKey}") (read-decimal 'amount${i}))`;
   }
 
@@ -37,18 +52,21 @@ export const generateTransaction = async (formData) => {
     const transferAmount = splitAmount[i] || splitAmount;
     pactBuilder.addData(`amount${i}`, transferAmount.toString());
   }
-  
+
+  // Calculate the gas limit based on the number of payees
+  const gasLimit = Math.min(700 * numPayees, 150000);
   const transaction = await pactBuilder
     .addSigner(senderPubKeys, (signFor) => [
       signFor(`${fungible}.GAS`),
       ...payeeArray.map((payee, index) => {
-        // const pubKey = payee.startsWith("k:") ? payee.slice(2) : "";
-        return signFor(`${fungible}.TRANSFER`, sender, payee, { decimal: (splitAmount[index] || splitAmount).toString() });
+        return signFor(`${fungible}.TRANSFER`, sender, payee, {
+          decimal: (splitAmount[index] || splitAmount).toString(),
+        });
       }),
     ])
     .setMeta({
       chainId: String(chain),
-      gasLimit: 2000,
+      gasLimit: gasLimit,
       gasPrice: 1.0e-6,
       ttl: 86400,
       senderAccount: sender,
@@ -56,7 +74,6 @@ export const generateTransaction = async (formData) => {
     .setNetworkId(network)
     .createTransaction();
 
- 
   const sigs = {
     ...senderPubKeys.reduce((acc, key) => {
       acc[key] = null;
@@ -64,13 +81,37 @@ export const generateTransaction = async (formData) => {
     }, {}),
   };
 
+  const preflightResult = await pc.dirtyRead(transaction);
+
+  if (preflightResult.result.status === "failure") {
+    alert(preflightResult.result.error.message);
+    throw new Error("command failure");
+  }
+
+  // console.log("preflight successful");
+
   return { ...transaction, sigs };
 };
 
 export const generateSafeTransferTransactions = async (formData) => {
-  const { payees, sender, chain, network, splitOption, amount, selectedKeys, fungible } = formData;
-
-  const payeeArray = payees.split("\n");
+  const {
+    payees,
+    sender,
+    chain,
+    network,
+    splitOption,
+    amount,
+    selectedKeys,
+    fungible,
+  } = formData;
+  const apiUrl =
+    network === "testnet04"
+      ? "https://api.testnet.chainweb.com"
+      : "https://api.chainweb.com";
+  const pc = createClient(
+    `${apiUrl}/chainweb/0.0/${network}/chain/${chain}/pact`
+  );
+  const payeeArray = payees.split("\n").filter((payee) => payee.trim() !== "");
   const numPayees = payeeArray.length;
   let splitAmount;
 
@@ -81,13 +122,15 @@ export const generateSafeTransferTransactions = async (formData) => {
     splitAmount = amountArray.map(parseFloat);
   }
 
-  const senderPubKeys = sender.startsWith("k:") ? [sender.slice(2)] : selectedKeys || [];
+  const senderPubKeys = sender.startsWith("k:")
+    ? [sender.slice(2)]
+    : selectedKeys || [];
 
   const transactions = await Promise.all(
     payeeArray.map(async (payee, index) => {
       const receiverPubKey = payee.startsWith("k:") ? payee.slice(2) : "";
       let transferAmount = splitAmount[index] || splitAmount;
-      transferAmount += 0.000000000001; 
+      transferAmount += 0.000000000001;
 
       const transaction = await Pact.builder
         .execution(
@@ -96,10 +139,14 @@ export const generateSafeTransferTransactions = async (formData) => {
         )
         .addSigner(senderPubKeys, (signFor) => [
           signFor(`${fungible}.GAS`),
-          signFor(`${fungible}.TRANSFER`, sender, payee, { decimal: transferAmount.toString() }),
+          signFor(`${fungible}.TRANSFER`, sender, payee, {
+            decimal: transferAmount.toString(),
+          }),
         ])
         .addSigner([receiverPubKey], (signFor) => [
-          signFor(`${fungible}.TRANSFER`, payee, sender, { decimal: "0.000000000001" }),
+          signFor(`${fungible}.TRANSFER`, payee, sender, {
+            decimal: "0.000000000001",
+          }),
         ])
         .addKeyset("payee", "keys-all", receiverPubKey)
         .addData("amount", transferAmount)
@@ -123,6 +170,12 @@ export const generateSafeTransferTransactions = async (formData) => {
         [receiverPubKey]: null,
       };
 
+      const preflightResult = await pc.dirtyRead(transaction);
+
+      if (preflightResult.result.status === "failure") {
+        alert(preflightResult.result.error.message);
+        throw new Error("command failure");
+      }
 
       return {
         cmd,
